a139d35de19f258f65b87068d018366925c5b162
MasvsaM <michu23serre@gmail.com>
2023-12-12 06:34:33 -0300

    detalle-curso

--- src/app/backend/detalle-curso/detalle-curso.component.ts
+++ src/app/backend/detalle-curso/detalle-curso.component.ts
@@ -4,15 +4,13 @@ import { FirebaseauthService } from 'src/app/services/firebaseauth.service';
 import { UtilsService } from 'src/app/services/utils.service';
 import { AlertController } from '@ionic/angular';
 import { Router } from '@angular/router';
-import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner/ngx';
-
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 @Component({
-  selector: 'app-detallecurso',
+  selector: 'app-detalle-curso',
   templateUrl: './detalle-curso.component.html',
   styleUrls: ['./detalle-curso.component.scss'],
 })
 export class DetalleCursoComponent implements OnInit {
-
   cursos: Curso[] = [];
   codigoQR: string = '';
   users: User[] = [];
@@ -25,8 +23,8 @@ export class DetalleCursoComponent implements OnInit {
     private utilsSvc: UtilsService,
     private alertController: AlertController,
     private router: Router,
-    private qrScanner: QRScanner
-  ) {}
+    private barcodeScanner: BarcodeScanner
+  ) { }
 
   ngOnInit() {
     this.firebaseauthSvc.stateUser().subscribe(res => {
@@ -51,25 +49,7 @@ export class DetalleCursoComponent implements OnInit {
     setTimeout(() => {
       this.codigoQR = `Curso: ${nombreCurso}, Sección: ${seccionCurso}, Fecha: ${new Date().toLocaleDateString()}`;
       this.loading = false;
-    }, 1300);  
-  }
-
-  scanQRCode() {
-    this.qrScanner.prepare()
-      .then((status: QRScannerStatus) => {
-        if (status.authorized) {
-          let scanSub = this.qrScanner.scan().subscribe((text: string) => {
-            console.log('Se escaneó algo', text);
-            this.onQRScan(text);
-            scanSub.unsubscribe();
-          });
-        } else if (status.denied) {
-          console.log('El permiso de la cámara fue denegado permanentemente');
-        } else {
-          console.log('Permiso denegado para esta ejecución');
-        }
-      })
-      .catch((e: any) => console.log('El error es', e));
+    }, 1300);
   }
 
   async presentAlertConfirm(nombreCurso: string, seccionCurso: string, fecha: string) {
@@ -93,15 +73,6 @@ export class DetalleCursoComponent implements OnInit {
       ]
     });
     await alert.present();
-  }
-
-  onQRScan(data: string) {
-    let info = this.decodeQRData(data);
-    this.presentAlertConfirm(info.nombreCurso, info.seccionCurso, info.fecha);
-
-    // Añade el usuario a la lista de alumnos presentes
-    let user = this.user();
-    this.addAlumnoPresente(user);
   }
 
   decodeQRData(data: string): QRInfo {
@@ -136,9 +107,9 @@ export class DetalleCursoComponent implements OnInit {
     this.firebaseauthSvc.getDocument(path).then((user: User) => {
       console.log('datos ->', user);
       if (user) {
-        this.rol = user.profile
+        this.rol = user.profile;
       }
-    })
+    });
   }
 
   getCursos() {
@@ -178,10 +149,34 @@ export class DetalleCursoComponent implements OnInit {
     };
   }
 
-  addAlumnoPresente(user: User) {
-    // Verifica si el usuario ya está en la lista
-    if (!this.alumnosPresentes.some(alumno => alumno.uid === user.uid)) {
-      this.alumnosPresentes.push(user);
+  scanBarcode() {
+    this.barcodeScanner.scan().then(barcodeData => {
+      console.log('Barcode data', barcodeData);
+      if (!barcodeData.cancelled) {
+        // Si se escanea un código sin cancelarse, realiza las acciones necesarias aquí
+        this.onBarcodeScan(barcodeData.text);
+      } else {
+        console.log('Escaneo cancelado');
+      }
+    }).catch(err => {
+      console.error('Error al escanear', err);
+    });
+  }
+  onBarcodeScan(data: string) {
+    let info = this.decodeQRData(data);
+    this.presentAlertConfirm(info.nombreCurso, info.seccionCurso, info.fecha);
+    let user = this.user();
+
+    if (user) {
+      // Verifica si el usuario ya está en la lista de presentes
+      if (!this.alumnosPresentes.some(a => a.uid === user.uid)) {
+        this.alumnosPresentes.push(user); // Agrega al usuario a la lista de presentes
+      } else {
+        console.log('El usuario ya está en la lista de presentes');
+      }
+    } else {
+      console.error('Usuario no autenticado');
     }
   }
 }
+
